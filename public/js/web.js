d59b0c6ed12234b5e4beeccc4a035d031f48df17
hoangsangskyred <https://github.com/hoangsangskyred>
2021-03-15 17:31:42 +0700

    demo

--- public/js/web.js
+++ public/js/web.js
@@ -18,3 +18,76 @@
             }, false)
         });
 })();
+
+
+// click button header
+
+function addClassOpen()
+{
+    let hamburger = document.querySelector('.hamburger')
+    let mobileNav = document.querySelector('.nav-list')
+    let bars = document.querySelectorAll('.hamburger span')
+    let isActive = false
+    if(window.innerHeight < window.innerWidth){
+        window.addEventListener('resize', function(){
+            addRequiredClass();
+        })
+        function addRequiredClass() {
+            if (window.innerWidth < 860) {
+                document.body.classList.add('mobile')
+            } else {
+                document.body.classList.remove('mobile') 
+            }
+        }
+        
+        window.onload = addRequiredClass
+
+        hamburger.addEventListener('click', function() {
+            mobileNav.classList.toggle('open')
+            if(!isActive) {
+                bars[0].style.transform = 'rotate(45deg)'
+                bars[1].style.opacity = '0'
+                bars[2].style.transform = 'rotate(-45deg)'
+                isActive = true
+            } else {
+                bars[0].style.transform = 'rotate(0deg)'
+                bars[1].style.opacity = '1'
+                bars[2].style.transform = 'rotate(0deg)'
+                isActive = false
+            }
+        })
+    
+    }
+    else
+    {
+        window.addEventListener('resize', function(){
+            addRequiredClass();
+        })
+        function addRequiredClass() {
+            if (window.innerWidth < 860) {
+                document.body.classList.add('mobile')
+            } else {
+                document.body.classList.remove('mobile') 
+            }
+        }
+        window.onload = addRequiredClass
+
+           hamburger.addEventListener('click', function() {
+            mobileNav.classList.toggle('open2')
+            if(!isActive) {
+                bars[0].style.transform = 'rotate(45deg)'
+                bars[1].style.opacity = '0'
+                bars[2].style.transform = 'rotate(-45deg)'
+                isActive = true
+            } else {
+                bars[0].style.transform = 'rotate(0deg)'
+                bars[1].style.opacity = '1'
+                bars[2].style.transform = 'rotate(0deg)'
+                isActive = false
+            }
+        })
+    }
+}
+$(document).ready(function (){
+  addClassOpen();
+});
--- resources/views/layouts/inc/top-nav.blade.php
+++ resources/views/layouts/inc/top-nav.blade.php
@@ -1,47 +1,39 @@
-<nav class="navbar navbar-expand-lg navbar-default navbar-dark">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="{{route('web.index')}}">
-            <img src="/public/img/logo_185x60.png">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('web.index')}}">Trang chủ</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('web.aboutUs')}}">Giới thiệu</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="{{route('web.services')}}" id="navbarServices" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dịch vụ
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarServices">
-                        @foreach(\App\Models\Service::whereShow('Y')->get() as $service)
-                        <li><a class="dropdown-item" href="{{route('web.service.detail', [$service->slug, 'dich-vu'])}}">{{$service->title}}</a></li>
-                        @endforeach
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarProjects" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dự án
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarProjects">
-                        <li><a class="dropdown-item" href="#">Biệt thự</a></li>
-                        <li><a class="dropdown-item" href="#">Căn hộ</a></li>
-                        <li><a class="dropdown-item" href="#">Nhà phố</a></li>
-                        <li><a class="dropdown-item" href="#">Văn hòng</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Blog</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Liên hệ</a>
-                </li>
-            </ul>
+<nav class="moblie">
+    <div class="container nav-wrapper">
+        <div class="brand">
+           <span><strong> <a class="navbar-brand" href="{{route('web.index')}}"><img src="/public/img/logo_185x60.png"></a></strong></span>
         </div>
+        <div class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+      
+        <ul class="nav-list">
+            <li>
+                <a href="{{route('web.index')}}">Trang chủ</a>
+            </li>
+            <li> <a href="{{route('web.aboutUs')}}">Giới thiệu</a></li>
+            <li>
+                <a href="#">Dịch Vụ <i class="fas fa-chevron-down"></i> </a>
+                <ul class="dropdown-list">
+                    @foreach(\App\Models\Service::whereShow('Y')->get() as $service)
+                    <li><a  href="{{route('web.service.detail', [$service->slug, 'dich-vu'])}}">{{$service->title}}</a></li>
+                    @endforeach
+                </ul>
+            </li>
+            <li>
+                <a href="#">Dự án <i class="fas fa-chevron-down"></i></a>
+                <ul class="dropdown-list">
+                    <li><a  href="#">Biệt thự</a></li>
+                    <li><a  href="#">Căn hộ</a></li>
+                    <li><a  href="#">Nhà phố</a></li>
+                    <li><a  href="#">Văn phòng</a></li>
+                </ul>
+            
+            </li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Liên hệ</a></li>
+        </ul>
     </div>
 </nav>
